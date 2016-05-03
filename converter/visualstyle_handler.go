458f3d9725b4265143f7a3273d31b54086d3543c
Keiichiro Ono <keiono@gmail.com>
2016-05-02 17:08:46 -0700

    Special handler for node size lock had been added

--- converter/visualstyle_handler.go
+++ converter/visualstyle_handler.go
@@ -3,6 +3,8 @@ package converter
 import (
 	cx "github.com/cytoscape-ci/cxtool/cx"
 	cyjs "github.com/cytoscape-ci/cxtool/cyjs"
+	"log"
+	"strconv"
 )
 
 const (
@@ -11,6 +13,16 @@ const (
 	cxEdges      string = "edges"
 	nodesDefault string = "nodes:default"
 	edgesDefault string = "edges:default"
+
+	// VP tags
+	dependencies string = "dependencies"
+	properties string = "properties"
+	propOf string = "properties_of"
+
+	// Visual Property Dependency List
+	nodeSizeLocked string = "nodeSizeLocked"
+	nodeCustomGraphicsSizeSync string = "nodeCustomGraphicsSizeSync"
+	arrowColorMatchesEdge string = "arrowColorMatchesEdge"
 )
 
 type VisualStyleHandler struct {
@@ -26,6 +38,7 @@ func (vsHandler VisualStyleHandler) HandleAspect(aspect []interface{}) map[strin
 	// Type converter
 //	vpConverter := VisualPropConverter{typeTable: vsHandler.typeTable}
 
+	// This is the number of elements in a VP section.
 	vpCount := len(aspect)
 
 	// Result Map
@@ -41,7 +54,17 @@ func (vsHandler VisualStyleHandler) HandleAspect(aspect []interface{}) map[strin
 	for i := 0; i < vpCount; i++ {
 		// Extract a new selector
 		vp := aspect[i].(map[string]interface{})
-		targetProperty := vp["properties_of"].(string)
+
+		targetProperty := vp[propOf].(string)
+
+		// Dependencies
+		var depList map[string]interface{}
+		deps, exist := vp[dependencies]
+		if exist {
+			depList = deps.(map[string]interface{})
+		}
+
+		log.Println(depList)
 
 		// Check valid graph object (node/edge/net) ot not
 		selectorTag, isDefaults := isValidProperty(targetProperty)
@@ -55,11 +78,46 @@ func (vsHandler VisualStyleHandler) HandleAspect(aspect []interface{}) map[strin
 
 		entry.Selector = selectorTag
 
-		cxProps := vp["properties"].(map[string]interface{})
+		// Properties list: Usually default values
+		cxProps := vp[properties].(map[string]interface{})
 
 		css := make(map[string]interface{})
 
+		// Handle Size:
+		sizeLocked := false
+
+		if depList != nil {
+			_, exist := depList[nodeSizeLocked]
+			if exist {
+
+				sizeLockedStr := depList[nodeSizeLocked].(string)
+				sizeLocked, _ = strconv.ParseBool(sizeLockedStr)
+
+				log.Println("LOCK:")
+				log.Println(sizeLocked)
+
+				if sizeLocked {
+					log.Println("THIS IS LOCKED:")
+					value := cxProps["NODE_SIZE"]
+					convertedValue := vsHandler.visualMappingGenerator.VpConverter.GetCyjsPropertyValue("NODE_SIZE", value.(string))
+					css["height"] = convertedValue
+					css["width"] = convertedValue
+				} else {
+					w := cxProps["NODE_WIDTH"]
+					h := cxProps["NODE_HEIGHT"]
+					wValue := vsHandler.visualMappingGenerator.VpConverter.GetCyjsPropertyValue("width", w.(string))
+					hValue := vsHandler.visualMappingGenerator.VpConverter.GetCyjsPropertyValue("height", h.(string))
+					css["height"] = hValue
+					css["width"] = wValue
+				}
+			}
+		}
+
 		for key, value := range cxProps {
+			if key == "NODE_WIDTH" || key == "NODE_HEIGHT" || key == "NODE_SIZE" {
+				continue
+			}
+
 			ag, exists := vsHandler.conversionTable[key]
 
 			if !exists {
@@ -99,6 +157,7 @@ func (vsHandler VisualStyleHandler) HandleAspect(aspect []interface{}) map[strin
 
 	return vpMap
 }
+
 
 
 func (vsHandler VisualStyleHandler) createMappings(selectorTag string,
